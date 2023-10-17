2d6dfe557361f2e5f70af0f7291f430f9e8dc9e4
Kunwar Sarthak Singh <kuwarsarthaksingh735@gmail.com>
2023-10-17 15:47:25 +0530

    Added city Service

---
+++ src/services/city-service.js
@@ -0,0 +1,43 @@
+const {CityRepository} = require('../repository/index');
+
+class CityService{
+    constructor(){
+        this.CityRepository = new CityRepository();
+    }
+
+    async createCity(data){
+        try {
+            const city = await this.CityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            throw{error};
+        }
+
+    }
+    async deleteCity(cityId){
+        try {
+            const response = await this.CityRepository.deleteCity(cityId);
+            return response;
+        } catch (error) {
+            throw{error};
+        }
+    }
+    async updateCity(cityId, data){
+        try {
+            const city = await this.CityRepository.updateCity(cityId, data);
+            return city;
+        } catch (error) {
+            throw{error};
+        }
+    }
+    async getCity(cityId){
+        try {
+            const city = await this.CityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            throw{error};
+        }
+    }
+}
+
+module.exports = CityService;
